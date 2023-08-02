eac8451df8057e6b8810e6917fda205b1e4dc2ce
Tamaraebi Dogubo <tdogubo@gmail.com>
2023-08-02 14:54:17 +0100

    feat: utils folder

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+// const apiKey = import.meta.env.VITE_API_KEY;
+// const requestUrl = import.meta.env.VITE_URL;
+
+/**
+ * @param {FormData} body
+ * @param {any} callback
+ */
+export default async function getResult(body, callback) {
+  // const response = await axios(requestUrl, {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "x-api-key": apiKey,
+  //   },
+  //   body,
+  //   method: "POST",
+  // });
+  console.log("response");
+}
