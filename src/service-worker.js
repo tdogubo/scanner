5d147f5dc650c6d2ec3f159dc05a782d1d9f8c7a
Tamaraebi Dogubo <tdogubo@gmail.com>
2023-08-11 19:06:26 +0100

    chore: background url check and notification WIP

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,37 +11,10 @@ async function getCurrentTab() {
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab.url;
 }
-
+let key = "";
 async function tabListener() {
-  const body = JSON.stringify({
-    client: {
-      clientId: "scanner_App",
-      clientVersion: "1.5.2",
-    },
-    threatInfo: {
-      threatTypes: ["MALWARE", "SOCIAL_ENGINEERING"],
-      platformTypes: ["WINDOWS"],
-      threatEntryTypes: ["URL"],
-      threatEntries: [
-        { url: "https://clicnews.com" },
-        { url: "https://tryteens.comm" },
-        {
-          url: "http://testsafebrowsing.appspot.com/apiv4/ANY_PLATFORM/MALWARE/URL/",
-        },
-      ],
-    },
-  });
   try {
-    const options = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      body,
-    };
-
-    const key = await fetch(
+    key = await fetch(
       "https://95qi9epou7.execute-api.us-east-1.amazonaws.com/default/fetchScanKey",
       { method: "POST" }
     )
@@ -52,39 +25,58 @@ async function tabListener() {
       .catch(() => {
         return "";
       });
-
-    const safetyCheck = await fetch(
-      `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${key}`,
-      options
-    );
-    const result = await safetyCheck.json();
-    console.log(result);
   } catch (err) {
-    console.error("ERROR:::", err);
+    console.error("Server Error");
   }
   await chrome.storage.sync.clear(() => {
     console.log("cleared");
   }); //! remove after test. This clears the storage.
+
   const tabUrl = await getCurrentTab();
   const tabsCache = await getTabs();
   if (
     !["", "chrome://extensions/"].includes(tabUrl) &&
     !tabsCache?.includes(tabUrl)
   ) {
-    const data = new FormData();
-    data.set("url", tabUrl);
-    getResult(tabUrl, (response) => {
-      console.log("API CALL RESPONSE ::", response);
+    const urlForm = new FormData();
+    urlForm.set("url", tabUrl);
+    let result = await getResult(tabUrl);
+
+    console.log(Object.keys(result));
+
+    if (Object.keys(result).length === 0) {
+      console.log("ALL GOOD");
+    } else {
+      // chrome.runtime.onMessage.addListener((data) => {
+      //   if (data.type === "notification") {
+      //     chrome.notifications.create("", data.options);
+      //   }
+      // });
+      // chrome.runtime.sendMessage("", {
+      //   type: "notification",
+      //   options: {
+      //     title: "Just wanted to notify you",
+      //     message: "How great it is!",
+      //     iconUrl: "/icon.png",
+      //     type: "basic",
+      //   },
+      // });
+      chrome.notifications.create(
+        "",
+        {
+          type: "basic",
+          iconUrl: "icon.png",
+          title: "This is a notification",
+          message: "hello there!",
+        },
+        function () {}
+      );
+    }
+
+    await chrome.storage.sync.set({
+      currentTab: tabUrl,
+      history: [...tabsCache, tabUrl],
     });
-    await chrome.storage.sync.set(
-      {
-        currentTab: tabUrl,
-        history: [...tabsCache, tabUrl],
-      },
-      () => {
-        console.log("Setting values");
-      }
-    );
   }
   await chrome.storage.sync.get(null, (data) => {
     console.log("VALUE SET", data);
@@ -97,19 +89,43 @@ export const checkUrl = async () => {
   return;
 };
 
-/**
- * @param {FormData} body
- * @param {any} callback
- */
-export default async function getResult(body, callback) {
-  // const response = await axios(requestUrl, {
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     "x-api-key": apiKey,
-  //   },
-  //   body,
-  //   method: "POST",
-  // });
-  // console.log("response", apiKey, requestUrl);
+export default async function getResult(tabUrl) {
+  let response = {};
+  try {
+    const body = JSON.stringify({
+      client: {
+        clientId: "scanner_App",
+        clientVersion: "1.5.2",
+      },
+      threatInfo: {
+        threatTypes: ["MALWARE", "SOCIAL_ENGINEERING"],
+        platformTypes: ["WINDOWS"],
+        threatEntryTypes: ["URL"],
+        threatEntries: [
+          { url: tabUrl },
+          {
+            url: "http://testsafebrowsing.appspot.com/apiv4/ANY_PLATFORM/MALWARE/URL/",
+          },
+        ],
+      },
+    });
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body,
+    };
+    const safetyCheck = await fetch(
+      `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${key}`,
+      options
+    );
+    response = { ...response, ...(await safetyCheck.json()) };
+  } catch (e) {
+    console.error("Server Error");
+  }
+  return response;
 }
+
 checkUrl();
